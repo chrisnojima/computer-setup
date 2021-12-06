5e8d286522eefe4c80d3d7c3e59f849ac104d863
chrisnojima <cnojima@keyba.se>
2021-12-06 12:42:41 -0500

    WIP

--- nvim/lua/basic.lua
+++ nvim/lua/basic.lua
@@ -1,90 +1,48 @@
 vim.opt.ai = true
 vim.opt.autoread = true
---- better backspace
-vim.opt.backspace = {"indent","start","eol"}
---- use system clipboard for yank
-vim.opt.clipboard = "unnamed"
---- set command height
-vim.opt.cmdheight = 2
---- show current lint
-vim.opt.cursorline = true
---- vertical diffs
-vim.opt.diffopt = "vertical"
---- tabs turn into spaces
-vim.opt.expandtab = true
---- acceptable file formats
-vim.opt.ffs = {"unix","dos","mac"}
---- how wide the fold column is
-vim.opt.foldcolumn = "0"
---- start unfolded
-vim.opt.foldlevelstart = 99
---- faster than syntax
-vim.opt.foldmethod = "indent"
---- hide buffers
-vim.opt.hidden = true
---- how much history to store
-vim.opt.history = 700
---- highlight as you search
-vim.opt.hlsearch = true
---- ignore case as you search
-vim.opt.ignorecase = true
---- realtime replace
-vim.opt.inccommand = "split"
---- incremental search
-vim.opt.incsearch = true
---- always show the statusbar
-vim.opt.laststatus = 2
---- buffers screen updates
-vim.opt.lazyredraw = true
---- linebreak
-vim.opt.lbr = true
---- magic regexp options
-vim.opt.magic = true
---- blink speed
-vim.opt.mat = 2
---- no backup files
-vim.opt.backup = false
---- no sounds
-vim.opt.errorbells = false
---- no backup files
-vim.opt.swapfile = false
---- no sounds
-vim.opt.visualbell = false
---- no backup files
-vim.opt.wb = false
---- show line numbers as hybrid relative
-vim.opt.rnu = true
---- tab width
-vim.opt.shiftwidth = 4
---- show current command on lower right, useful for leader
-vim.opt.showcmd = true
---- show matching brackets
-vim.opt.showmatch = true
+vim.opt.backspace = {"indent","start","eol"} --- better backspace
+vim.opt.clipboard = "unnamed" --- use system clipboard for yank
+vim.opt.cmdheight = 2 --- set command height
+vim.opt.cursorline = true --- show current lint
+vim.opt.diffopt = "vertical" --- vertical diffs
+vim.opt.expandtab = true --- tabs turn into spaces
+vim.opt.ffs = {"unix","dos","mac"} --- acceptable file formats
+vim.opt.foldcolumn = "0" --- how wide the fold column is
+vim.opt.foldlevelstart = 99 --- start unfolded
+vim.opt.foldmethod = "indent" --- faster than syntax
+vim.opt.hidden = true --- hide buffers
+vim.opt.history = 700 --- how much history to store
+vim.opt.hlsearch = true --- highlight as you search
+vim.opt.ignorecase = true --- ignore case as you search
+vim.opt.inccommand = "split" --- realtime replace
+vim.opt.incsearch = true --- incremental search
+vim.opt.laststatus = 2 --- always show the statusbar
+vim.opt.lazyredraw = true --- buffers screen updates
+vim.opt.lbr = true --- linebreak
+vim.opt.magic = true --- magic regexp options
+vim.opt.mat = 2 --- blink speed
+vim.opt.backup = false --- no backup files
+vim.opt.errorbells = false --- no sounds
+vim.opt.swapfile = false --- no backup files
+vim.opt.visualbell = false --- no sounds
+vim.opt.wb = false --- no backup files
+vim.opt.rnu = true --- show line numbers as hybrid relative
+vim.opt.shiftwidth = 4 --- tab width
+vim.opt.showcmd = true --- show current command on lower right, useful for leader
+vim.opt.showmatch = true --- show matching brackets
 --- from coc
 vim.opt.shortmess = vim.opt.shortmess + "c"
 vim.opt.signcolumn = "yes"
---- smart indent
-vim.opt.si = true
---- override ignorecase when you really want case
-vim.opt.smartcase = true
---- make tabs correct
-vim.opt.smarttab = true
---- spellcheck
-vim.opt.spell = true
+vim.opt.si = true --- smart indent
+vim.opt.smartcase = true --- override ignorecase when you really want case
+vim.opt.smarttab = true --- make tabs correct
+vim.opt.spell = true --- spellcheck
 vim.opt.suffixesadd = {".tsx", ".d.ts", ".js"}
---- tabs
-vim.opt.tabstop = 4
---- full colors
-vim.opt.termguicolors  = true
---- no sounds
-vim.opt.tm = 500
---- never linebreak
-vim.opt.tw = 0
---- faster refresh
-vim.opt.updatetime = 300
---- visual block anywhere
-vim.opt.virtualedit = "block"
----  visual autocomplete for command menu
-vim.opt.wildmenu  = true
---- wrap lines
-vim.opt.wrap  = true
+vim.opt.tabstop = 4 --- tabs
+vim.opt.termguicolors  = true --- full colors
+vim.opt.tm = 500 --- no sounds
+vim.opt.tw = 0 --- never linebreak
+vim.opt.updatetime = 300 --- faster refresh
+vim.opt.virtualedit = "block" --- visual block anywhere
+vim.opt.wildmenu  = true ---  visual autocomplete for command menu
+vim.opt.wrap  = true --- wrap lines
---
+++ nvim/lua/plug.lua
@@ -0,0 +1,35 @@
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin', '~/.config/nvim/plugged')
+
+Plug 'airblade/vim-gitgutter' --- show git changes in the gutter
+Plug 'luochen1990/rainbow' ---  better rainbow
+Plug 'nathanaelkane/vim-indent-guides' --- visually shows indents
+Plug 'itchyny/lightline.vim' --- status bar / tabs
+Plug 'roman/golden-ratio' --- keep window sizes better
+Plug('vim-scripts/matchit.zip', { ['for'] = 'javascript' }) --- smarter %
+Plug 'justinmk/vim-sneak' ---  better movement
+
+--- Search
+Plug 'jremmen/vim-ripgrep' ---  better search
+Plug('junegunn/fzf', {dir= '~/.fzf', ['do']= './install --all' })
+Plug 'junegunn/fzf.vim' --- fuzzy finder
+
+--- Files
+Plug('scrooloose/nerdtree', {on = {'NERDTreeToggle', 'NERDTreeFind'} }) --- file explorer
+Plug('Xuyuanp/nerdtree-git-plugin', {on = {'NERDTreeToggle', 'NERDTreeFind'} })  ---  git aware nerdtree
+
+--- Utility
+Plug 'tpope/vim-abolish' --- smarter substitute and abbreviate
+Plug 'tpope/vim-fugitive' --- git integration
+Plug 'tpope/vim-repeat' --- repeat commands better
+Plug 'tpope/vim-surround' --- surround things better
+Plug 'tpope/vim-unimpaired' --- toggle mappings quicker
+Plug 'scrooloose/nerdcommenter' --- better comments
+Plug 'mhartington/oceanic-next' ---  colors
+Plug 'sheerun/vim-polyglot' --- various language support (js etc)
+Plug 'sjl/gundo.vim' --- viz undo tree
+Plug('neoclide/coc.nvim', {branch = 'release'})
+Plug 'Raimondi/delimitMate' --- closes brackets
+
+vim.call('plug#end')
