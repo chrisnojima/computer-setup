16dbb508ba1d50714722817fc79cac78df223270
chrisnojima <cnojima@keyba.se>
2023-10-16 15:14:34 -0400

    WIP

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -5,7 +5,26 @@ return {
     opts = function(_, opts)
       local icons = require("lazyvim.config").icons
       local Util = require("lazyvim.util")
+      opts.sections.lualine_a = {
+        function()
+          local mode = require("lualine.utils.mode").get_mode()
+          return string.sub(mode, 1, 1)
+        end,
+      }
+      opts.sections.lualine_c = {
+        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+        { Util.lualine.pretty_path() },
+      }
       opts.sections.lualine_x = {
+        {
+          "diagnostics",
+          symbols = {
+            error = icons.diagnostics.Error,
+            warn = icons.diagnostics.Warn,
+            info = icons.diagnostics.Info,
+            hint = icons.diagnostics.Hint,
+          },
+        },
         {
           function()
             return require("noice").api.status.mode.get()
