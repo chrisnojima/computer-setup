34258d1861516ea3647aa184b40c4ebb725b4309
chrisnojima <cnojima@keyba.se>
2024-02-08 16:06:13 -0500

    WIP

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,12 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- fix netrw eating shift-h and shift-l
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "netrw" },
+  callback = function()
+    vim.api.nvim_buf_set_keymap(0, "n", "<S-h>", "<cmd>bprevious<cr>", { silent = false })
+    vim.api.nvim_buf_set_keymap(0, "n", "<S-l>", "<cmd>bnext<cr>", { silent = false })
+  end,
+})
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -3,38 +3,12 @@ return {
     "nvim-telescope/telescope.nvim",
     keys = {
       {
-        "<leader>sf",
-        function()
-          require("telescope.builtin").find_files({})
-        end,
-        desc = "Grep",
-      },
-      {
-        "<leader>st",
-        function()
-          require("telescope.builtin").live_grep({})
-        end,
-        desc = "Grep",
-      },
-      {
-        "<leader>su",
-        "<cmd>Telescope resume<CR>",
-        desc = "Last search",
-      },
-      {
         "<leader>sd",
         function()
           require("telescope.builtin").diagnostics({ default_text = ":error:" })
         end,
         "<cmd>Telescope resume<CR>",
         desc = "LSP error",
-      },
-      {
-        "<leader>sw",
-        function()
-          require("telescope-live-grep-args.shortcuts").grep_word_under_cursor({ postfix = " -F -i" })
-        end,
-        "Search word under cursor",
       },
     },
     opts = function()
